bd4942aef3be461e28d66cbc5344a742b9ff2f0b
doggbmx <manuelorticosta@gmail.com>
2023-07-25 23:07:26 -0400

    checkpoint: sleepy time

---
+++ src/closure/lexical.js
@@ -0,0 +1,4 @@
+// permiten acceder al ambito de una funcion exterior desde una funcion interior.
+// En JS, las clausulas son creadas cada vez que una funcion es creada
+// No son siempre utilizados. Se utilizan cuando se crea una funcion que es retornada por otra funcion.
+// Tienes un closure cuando una funcion cualquiera accede a una variable fuera de su contexto y la recuerda
