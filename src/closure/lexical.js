2e28ddb4a1a2bc5f71389693185683db3d0ee9d2
doggbmx <manuelorticosta@gmail.com>
2023-07-28 11:11:27 -0400

    end class 8

--- src/closure/lexical.js
+++ src/closure/lexical.js
@@ -85,3 +85,20 @@ const exito = crearImpresoraDeMensajes(
 error("Algo salió mal");
 warning("Algo salió mal");
 exito("Algo salió bien");
+
+// EJERCICIO DE PLATZI
+// En este desafío recibirás dos números aleatorios por parámetros.
+//Usando las closures deberás crear una función a la cual se pueda llamar en primer lugar
+//con un solo número y secuencialmente volver a llamarla para completar la suma con el siguiente número:
+//La solución deberá tener un input y output como la siguiente,
+//en algunos casos puede que no se mande el segundo número por lo que deberás pensar en como manejar ese tipo de casos.
+
+export function sumWithClosure(firstNum) {
+  // Tu código aquí 👈
+  return function sum(secondNum) {
+    if (!secondNum) {
+      return firstNum;
+    }
+    return firstNum + secondNum;
+  };
+}
