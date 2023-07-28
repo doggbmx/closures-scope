377f98963766db70ff59a6d13436a36031562e5d
doggbmx <manuelorticosta@gmail.com>
2023-07-28 09:59:06 -0400

    end class 7

--- src/closure/lexical.js
+++ src/closure/lexical.js
@@ -2,3 +2,26 @@
 // En JS, las clausulas son creadas cada vez que una funcion es creada
 // No son siempre utilizados. Se utilizan cuando se crea una funcion que es retornada por otra funcion.
 // Tienes un closure cuando una funcion cualquiera accede a una variable fuera de su contexto y la recuerda
+const myGlobal = 0;
+
+function myFunction() {
+  const myNumber = 1;
+  console.log(myGlobal);
+
+  function parent() {
+    // funcion interna
+    const inner = 2;
+    console.log(myNumber);
+    console.log(myGlobal);
+
+    function child() {
+      console.log(inner);
+      console.log(myNumber);
+      console.log(myGlobal);
+    }
+    return child();
+  }
+  return parent();
+}
+
+myFunction();
