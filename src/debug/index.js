83e4d3220b52ee054e1d1333fb819e5a3ab0443e
doggbmx <manuelorticosta@gmail.com>
2023-07-28 13:35:02 -0400

    end of course

---
+++ src/debug/index.js
@@ -0,0 +1,11 @@
+var a = "Hello";
+
+function hello() {
+  let b = "Hello World";
+  const c = "Hello World!";
+  debugger;
+}
+
+hello();
+
+// hay un curso de Platzi especifico para el uso de debugger
